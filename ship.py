73cfe90c8115278c27bdda3b7a59d9442d8d2f80
tebii <105075744+tebii@users.noreply.github.com>
2024-08-09 20:09:23 +0800

    Add ship controls

--- ship.py
+++ ship.py
@@ -7,12 +7,30 @@ class Ship:
         """Initialize the ship and sets its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen_rect
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+        
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's value, not the rect
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
