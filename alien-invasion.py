588fa95f7f5271e394eea14582f371d52d25fe1d
tebii <105075744+tebii@users.noreply.github.com>
2024-08-09 21:25:47 +0800

    Refactor update bullets

--- alien-invasion.py
+++ alien-invasion.py
@@ -26,12 +26,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            # Get rid of bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # Redraw the screen during each pass through the loop
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -67,6 +62,17 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions
+        self.bullets.update()
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
