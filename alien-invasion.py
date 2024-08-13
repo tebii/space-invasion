69aa1227e4bed8dd8cc60a47749850fb4b5c9138
tebii <105075744+tebii@users.noreply.github.com>
2024-08-13 17:03:39 +0800

    Add create alien

--- alien-invasion.py
+++ alien-invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
@@ -17,6 +18,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
  
         # Set the background color
         self.bg_color = (self.settings.bg_color)
@@ -78,7 +81,28 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
+
+    def _create_fleet(self):
+        """Create a fleet of aliens."""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # first row of aliens
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number) 
+
+    def _create_alien(self, alien_number):
+        """Create an alien and place it in the row."""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
