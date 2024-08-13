bc68f8819e8a239b1a9feba50127e40e917e6427
tebii <105075744+tebii@users.noreply.github.com>
2024-08-13 18:00:41 +0800

    Add drop the fleet and change direction

--- alien.py
+++ alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Initialize the alien and sets its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
@@ -19,3 +20,15 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Return true if alien is at edge of screen."""
+        self_rect = self.screen.get_rect()
+        if self.rect.right >= self_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien to the right or right."""
+        self.x += (self.settings.alien_speed *
+                        self.settings.fleet_direction)
+        self.rect.x = self.x
