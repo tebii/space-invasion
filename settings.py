24be879f070619518b8bad42c77e8f4babf65c95
tebii <105075744+tebii@users.noreply.github.com>
2024-08-14 16:05:47 +0800

    Drop fleet and change direction

--- settings.py
+++ settings.py
@@ -16,8 +16,8 @@ class Settings:
         self.bullet_color = (30, 30, 30)
         self.bullets_allowed = 3
         # alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 0.3
+        self.fleet_drop_speed = 10 
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
