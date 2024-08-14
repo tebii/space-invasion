ba5762276b8e1ae00f17c58f1042399b7c09ab8e
tebii <105075744+tebii@users.noreply.github.com>
2024-08-14 16:43:17 +0800

    Add game end when ship limit is reached

--- alien-invasion.py
+++ alien-invasion.py
@@ -33,9 +33,11 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -98,20 +100,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship beging hit by an alien"""
+        if self.stats.ships_left > 0:
+            # Decrement ship_left
+            self.stats.ships_left -= 1
 
-        # Decrement ship_left
-        self.stats.ships_left -= 1
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Get rid of any remaining aliens and bullets
-        self.aliens.empty()
-        self.bullets.empty()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
-
-        # Pause
-        sleep(0.5)
+            # Pause
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen"""
--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
         """Initialize statistics"""
         self.settings = ai_game.settings
         self.reset_stats()
+        # Start the alien invasion in an active state
+        self.game_active = True
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
