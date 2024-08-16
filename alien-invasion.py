86da10c94037ea930cb34ec38a0d1e3b9a94a2ed
tebii <105075744+tebii@users.noreply.github.com>
2024-08-16 16:17:45 +0800

    Add ship lives

--- alien-invasion.py
+++ alien-invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
             self.bullets.empty()
@@ -143,6 +144,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ship_left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
